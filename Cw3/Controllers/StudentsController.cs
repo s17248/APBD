56e754b00dc3f92a31a503a6d386ee946581aaca
Agata Dopart <s17248@pjwstk.edu.pl>
2020-04-05 15:59:49 +0200

    Import rozwiązania dla Cw3

---
+++ Cw3/Controllers/StudentsController.cs
@@ -0,0 +1,45 @@
+﻿using Cw3.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Cw3.DAL;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Cw3.Controllers
+{
+    [ApiController]
+    [Route("api/students")]
+    public class StudentsController : ControllerBase
+    {
+        private readonly IDbService _dbService;
+
+        public StudentsController(IDbService dbService)
+        {
+            _dbService = dbService;
+        }
+        [HttpGet]
+        public IActionResult GetStudents(string orderBy)
+        {
+            return Ok(_dbService.GetStudents());
+        }
+        [HttpPost]
+        public IActionResult CreateStudent(Student student)
+        {
+            student.IndexNumber = $"s{new Random().Next(1, 20000)}";
+            return Ok(student);
+        }
+        [HttpPut]
+        public IActionResult CreateStudent2(int id)
+        {
+            return Ok("Aktualizacja zakończona");
+        }
+        [HttpDelete]
+        public IActionResult DeleteStudent(int id)
+        {
+            return Ok("Usuwanie zakończone");
+        }
+    }
+}
---
+++ Cw3/DAL/IDbService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Cw3.Models;
+using System.Collections.Generic;
+
+
+namespace Cw3.DAL
+{
+    public interface IDbService
+    {
+        public IEnumerable<Student> GetStudents();
+    }
+}
